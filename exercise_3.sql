2c2b1d02a1e86160d023894b5488d428a1cd5069
fhmyazz <fahmyaziz1805@gmail.com>
2026-05-11 21:52:07 +0700

    re-push

---
+++ exercise_3.sql
@@ -0,0 +1,24 @@
+with completed_trx as(
+	select
+		   rt.id trx_id,
+		   rt.user_id, p.category, rt.amount, p.cost_price,
+		   rt.amount - p.cost_price profit,
+		   row_number() over(
+		   		partition by rt.user_id, p.category, rt.amount
+		   ) as rownum_unique
+	from raw_transactions rt,
+		 products p
+	where rt.product_id = p.id
+	and   rt.status = 'completed'
+), with_dedup as(
+	select *
+	from completed_trx
+	where rownum_unique = 1
+), with_avg_profit as(
+	select trx_id, user_id, category, amount, cost_price, profit,
+			round(avg(profit) over(), 1) avg_profit
+	from with_dedup
+)
+select trx_id, user_id, category, amount, cost_price, profit
+from with_avg_profit wap
+where profit > avg_profit 
